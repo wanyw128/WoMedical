2e2e3e900982e3f03dc303094b2e5ab9874e0386
VanesssaLei <botchedbunnies@gmail.com>
2023-09-13 19:40:46 -0500

    Development - EventsPage1
    Created basic layout with header, timeline, popup gallery, and scroll.

---
+++ events.js
@@ -0,0 +1,22 @@
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const leftButton = document.getElementById("slide-arrow-left");
+const rightButton = document.getElementById("slide-arrow-right");
+const photoContainers = document.querySelectorAll('.expand');
+
+rightButton.addEventListener("click", (event) => {
+    const slideWidth = slide.clientWidth;
+      slidesContainer.scrollLeft += slideWidth;
+  });
+
+leftButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
+  });
+
+photoContainers.forEach(container => {
+    container.addEventListener('click', function() {
+      // Toggle the expanded class on the clicked container
+      this.classList.toggle('expanded');
+    });
+});
